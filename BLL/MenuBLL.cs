ef1d059e258e5df59df0bef77270c9d3e034a997
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-19 19:25:25 -0400

    Añadiendo a la BLL el menu

---
+++ BLL/MenuBLL.cs
@@ -0,0 +1,11 @@
+﻿using PizzaPlaceApp.Models;
+
+namespace PizzaPlaceApp.BLL
+{
+    public class MenuBLL
+    {
+        public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
+        public void Add(Pizzas pizza) => Pizzas.Add(pizza);
+        public Pizzas? GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
