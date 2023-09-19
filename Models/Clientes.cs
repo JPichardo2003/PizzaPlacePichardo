5030c84a718b7324f27fbdf7a266c7ed81a34b05
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-19 19:25:07 -0400

    Añadiendo en la carpeta models: CestaCompras, clientes y estados

---
+++ Models/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlaceApp.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor proporcione su nombre")]
+        [StringLength(30, ErrorMessage = "No puedes exceder los 30 caracteres")]
+        public string Nombre { get; set; } = default!;
+
+        [Required(ErrorMessage = "Por Favor proporcione su Calle con el Numero de Casa")]
+        public string Calle { get; set; } = default!;
+
+        [Required(ErrorMessage = "Por Favor Proporcione una ciudad")]
+        public string Ciudad { get; set; } = default!;
+    }
+}
---
+++ Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlaceApp.BLL;
+
+namespace PizzaPlaceApp.Models
+{
+    public class Estados
+    {
+        public MenuBLL Menu { get; } = new MenuBLL();
+        public CestaCompras Cesta { get; } = new CestaCompras();
+        public InterfazUsuarios Interfaz { get; set; } = new InterfazUsuarios();
+        public decimal PrecioTotal => Cesta.Pedidos.Sum(Id => Menu.GetPizza(Id)!.Precio);
+    }
+}
